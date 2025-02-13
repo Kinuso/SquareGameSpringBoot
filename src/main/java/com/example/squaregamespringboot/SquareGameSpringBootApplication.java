71c92620b7d94f7e84b2952ec0f437263c702e9e
Kinuso <killian2908@gmail.com>
2025-02-13 10:00:24 +0100

    Spring boot + Maven project init

---
+++ src/main/java/com/example/squaregamespringboot/SquareGameSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.squaregamespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGameSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquareGameSpringBootApplication.class, args);
+    }
+
+}
